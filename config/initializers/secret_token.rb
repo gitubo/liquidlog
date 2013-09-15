25faa58669cdf8d720dc03420645826fd59e2024
Massimiliano Mosca <mosca.massimiliano@gmail.com>
2013-09-15 21:22:56 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Liquidlog::Application.config.secret_token = 'a0d3a26cb52062d320958406bcef505ba7ba1857cc3684ec3f6894ab140f6f0af56a63e3ec842bfafd0e239c0e52a4c0db1754063ade64fc0a71d28c3959139f'
