be5e6218aa8974442561dac5a186df79d8e39f07
Massimiliano Mosca <mosca.massimiliano@gmail.com>
2013-09-15 21:51:15 +0200

    Initial scheleton

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+	def full_title(base_title, page_title)
+		if base_title.empty?
+			base_title = "LiquidLog"
+		end
+		if page_title.empty?
+			base_title
+		else
+			"#{base_title} | #{page_title}"
+		end
+	end
+
 end
---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+	let(:base_title) { "LiquidLog" }
+	subject { page }
+
+	describe "Home page" do
+		before { visit root_path }
+		it { should have_selector('h1', text: 'LiquidLog') }
+	    it { should have_selector 'title',
+                        text: "LiquidLog" }
+    	it { should_not have_selector 'title', text: '| Home' }
+	end
+
+	describe "Help page" do
+		before { visit help_path }
+		it { should have_selector('h1', text: 'Help') }
+		it { should have_selector(
+				'title', 
+				:text => "#{base_title} | Help") }
+	end
+
+	describe "About page" do
+		before { visit about_path }
+		it { should have_selector('h1', text: 'About Us') }
+		it { should have_selector(
+				'title', 
+				:text => "#{base_title} | About Us") }
+	end
+
+	describe "Contact page" do
+		before { visit contact_path }
+		it { should have_selector('h1', text: 'Contact') }
+		it { should have_selector('title', 
+				:text => "#{base_title} | Contact") }
+  	end
+
+end
