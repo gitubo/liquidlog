7a6303145245c6d46fa9a80b6a56099ab81a464c
Massimiliano Mosca <mosca.massimiliano@gmail.com>
2013-09-20 14:06:42 +0200

    Punto di salvataggio

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Liquidlog::Application.routes.draw do
-  get "static_pages/home"
-
-  get "static_pages/help"
-
-  get "users/new"
+  resources :users
 
   root              to: 'static_pages#home'
   match '/help',    to: 'static_pages#help'
