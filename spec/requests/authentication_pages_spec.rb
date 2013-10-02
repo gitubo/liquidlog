2d730274f9f940097203609f3e229e036b7d1d1a
Massimiliano Mosca <mosca.massimiliano@gmail.com>
2013-10-02 16:00:31 +0200

    Finish update user

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -5,17 +5,17 @@ describe "Authentication" do
 	subject { page }
   	
   	describe "signin page" do
-    	before { visit signin_page }
+    	before { visit signin_path }
 
     	it { should have_selector('h1'   , text: 'Sign in') }
     	it { should have_selector('title', text: 'Sign in') }
     end
 
 	describe "signin" do
-    	before { visit signin_page }
+    	before { visit signin_path }
 
     	describe "with invalid information" do
-	    	before { click_button "Sing in" }
+	    	before { click_button "Sign in" }
 
 	    	it { should have_selector('title', text: 'Sign in') }
     		it { should have_selector('div.alert.alert-error', text: 'Invalid') }
@@ -28,14 +28,11 @@ describe "Authentication" do
 
 		describe "with valid information" do
 	    	let(:user) {FactoryGirl.create(:user) }
-	    	before do
-	    		fill_in "Email", with: user.email
-	    		fill_in "Password", with: user.password
-	    		click_button "Sign in"
-	    	end
+	    	before { sign_in user }
 
 	    	it { should have_selector('title', text: user.name) }
-	    	it { should have_link('Profile', href: user.path(user)) }
+	    	it { should have_link('Profile', href: user_path(user)) }
+	    	it { should have_link('Settings', href: edit_user_path(user)) }
 	    	it { should have_link('Sign out', href: signout_path) }
     		it { should_not have_link('Sign in', href: signin_path) }
 
